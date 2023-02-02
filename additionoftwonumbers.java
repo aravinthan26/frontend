4def5e04d6203b70b1ff34dd1118ea6e08c6bec1
aravinthan26 <124272325+aravinthan26@users.noreply.github.com>
2023-02-02 23:12:10 +0530

    Create additionoftwonumbers.java

---
+++ additionoftwonumbers.java
@@ -0,0 +1,11 @@
+package basicsofjava;
+
+	public class Additiontwointeger {
+		public static void main(String[] args) {
+			int a=19;
+			int b=26;
+			int c=a+b;
+			System.out.println(c);
+}
+
+}
